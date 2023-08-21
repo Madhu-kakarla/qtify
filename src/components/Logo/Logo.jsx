168a902e89484254243d6982ee71c0afbd12ddff
Madhu Kakarla <madhuwebdev7@gmail.com>
2023-08-21 22:09:16 +0530

    add home link to logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import LogoImage from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <img src={LogoImage} alt='Logo' width={67} />
+    <Link to='/' title='Home'><img src={LogoImage} alt='Logo' width={67} /></Link>
   )
 }
 
